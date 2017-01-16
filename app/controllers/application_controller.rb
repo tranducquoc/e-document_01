f3d772c2e3877151e1fa352200777c91b1979977
buiquyhoat <buiquyhoat1@gmail.com>
2017-01-16 09:30:22 +0700

    multi-language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+  include ApplicationHelper
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 
   rescue_from CanCan::AccessDenied do |_exception|
     flash[:alert] = t "access_denied"
@@ -8,5 +14,10 @@ class ApplicationController < ActionController::Base
 
   def render_404
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
