d323bd70d47f9efc1882efb823332b02ab832dc2
buiquyhoat <buiquyhoat1@gmail.com>
2017-01-16 16:50:11 +0700

    soft-delete-user-delete-doc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+  before_action :load_document, only: [:show, :destroy]
 
   def index
     @q = User.ransack params[:q]
     @users = @q.result(distinct: true).order("created_at DESC")
       .page(params[:page]).per Settings.users.per_page
+  end
+
+  def show
+    @documents = @user.documents.order(created_at: :desc).page params[:page]
+  end
+
+  private
+  def load_document
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash.now[:warning] = t "user.not_found"
+      render_404
+    end
   end
 end
