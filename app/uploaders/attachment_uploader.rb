c35fae25b712ee1c9beb1ea6f8d4e1080c6aa6ea
buiquyhoat <buiquyhoat1@gmail.com>
2017-02-13 16:00:35 +0700

    get the first page to the thumbnail

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,9 +1,9 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
   if Rails.env.production?
     storage :fog
   else
     storage :file
-
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
@@ -12,5 +12,19 @@ class AttachmentUploader < CarrierWave::Uploader::Base
       %w(pdf doc htm html docx)
     end
   end
+  def cover
+    manipulate! do |frame, index|
+      frame if index.zero? # take only the first page of the file
+    end
+  end
 
+  version :preview do
+    process :cover
+    process resize_to_fit: [310, 280]
+    process convert: :jpg
+
+    def full_filename (for_file = model.source.file)
+      super.chomp(File.extname(super)) + Settings.jpg
+    end
+  end
 end
